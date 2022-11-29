5569ce1f20d545a44fc1539d9d27a3c979857ea9
Francisca <francisca.orwa@student.moringaschool.com>
2022-11-29 12:39:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-function addingEventListener() {
+//const input = document.getElementById('input');
+
+//function clickAlert() {
+  //alert('I was clicked!');
+//}
+
+//input.addEventListener('click', clickAlert);
+//const input=document.getElementById('input')
+//function addingEventLisner(){
+ //   alert('I was clicked!')
+//}
+
+//input.addEventListener('click',addingEventLisner)
+function addingEventListener(){
+    const input = document.getElementById('input');
+input.addEventListener('click',function(){
+    alert('I was clicked!');
+});    
 }
